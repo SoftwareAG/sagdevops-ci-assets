2cedddcfed42d69dc9ed877e42054ba171192624
Henning Waack <henning.waack@gmx.de>
2017-04-26 13:59:30 +0200

    added capability to split deployments into multiple deplyoment sets

--- resources/vs/CreateVarSub.groovy
+++ resources/vs/CreateVarSub.groovy
@@ -29,10 +29,10 @@ def cli = new CliBuilder (usage:'createdeployerproject.groovy -r REPOSITORY_PATH
 cli.with {
 	h longOpt:'help', 'Usage information'
 	a longOpt:'a',argName:'paTemplate', args:1, 'Path to project automator template file'
-	f longOpt:'outputfile', argName:'outputfile', args:1, 'Example: projectAutomator.xml'
 	p longOpt:'project', argName:'project', args:1, 'Example: BDA_Automator_Project'
 	t longOpt:'target', argName:'target', args:1, 'Must have a matching entry in the ENV.groovy flie. Example: DEV.'
 	vs longOpt:'varSubDirPath', argName:'varSubDirPath', args:1, 'Base dir with the varsub files'
+	o longOpt:'varSubOutputDirPath', argName:'varSubOutputDirPath', args:1, 'Output directory for varsub files'
 }
 def opts = cli.parse(args)
 org.codehaus.groovy.ant.Groovy
@@ -45,91 +45,111 @@ if(!opts) return
 
 assert opts
 assert opts.a
-assert opts.f
 assert opts.p
 assert opts.t
 assert opts.vs
+assert opts.o
 
 
 def paTemplate = opts.a
-def outputFile = opts.f
 def project = opts.p
 def target = opts.t
 def varSubDirPath = opts.vs
+def varSubOutputDirPath = opts.o
 
 println "Creating VarSub file with the following properties:"
 println "\t- Project: '${project}'"
 println "\t- Project Automator Template: '${paTemplate}'"
-println "\t- Output File: '${outputFile}'"
 println "\t- Target: '${target}'"
 println "\t- Varsub base dir: '${varSubDirPath}'"
+println "\t- varSubOutputDirPath: '${varSubOutputDirPath}'"
 
 def currentDir = new File(".")
 def varSubDir = new File(varSubDirPath)
 def varSubEnvDir = new File(varSubDir, target)
 assert varSubDir.exists() : "varSubDir for path '${varSubDirPath}' does not exist. Root dir: ${currentDir.getAbsolutePath()}"
-if( !varSubEnvDir.exists()) 
+if( !varSubEnvDir.exists()) {
 	println "varSubDir for environment '${target}' does not exist. Expected directory at: '${varSubDir.getAbsolutePath()}/${target}'. No variable substitution will be done..."
-
-PrintWriter outputFileWriter = new PrintWriter(new BufferedWriter(new FileWriter(outputFile)));
-def varSubXml = new StreamingMarkupBuilder()
-def packages = []
-def bpms = []
-def mws = []
-def tn = []
-def isTargets = []
-def bpmsTargets = []
-def mwsTargets = []
-
+	return;
+}
+def varSubOutputDir = new File(varSubOutputDirPath)
+if( !varSubOutputDir.exists() ) {
+	varSubOutputDir.mkdirs()
+}
 def paTemplateXml = new XmlSlurper().parse(new File(paTemplate))
+
+def varSubEnvDirs = ["IS": new File(varSubEnvDir, "is"), "BPM": new File(varSubEnvDir, "bpm"), "MWS": new File(varSubEnvDir, "mws")]
+
+//Map targetsIs = { [:].withDefault{ owner.call() } }()
+def deploymentMaps = [:]
+def deploymentSets = [:]
+
 // parse project automator template and extract all composites
-def DeploymentSet = paTemplateXml.Projects.Project.DeploymentSet
-DeploymentSet.Composite.findAll {true}.each { Composite ->
-	if( Composite.@type == 'IS') {
-//		println("found IS package composite" + XmlUtil.serialize(Composite))
-		packages.add([name: Composite.@name, srcAlias: Composite.@srcAlias])
+paTemplateXml.Projects.Project.MapSetMapping.findAll{true}.each { msm ->
+	String dsName = msm.@setName
+	String mapName = msm.@mapName
+	deploymentMaps[mapName] = []
+	msm.alias.findAll{true}.each { alias ->
+		String aliasName = alias.text()
+		String aliasType = alias.@type
+		println "found alias '${aliasName}' of type '${aliasType}' for DeploymentSet '${dsName}'"
+		deploymentMaps[mapName].add([dsName: dsName, targetServerName: aliasName, targetServerType: aliasType])
 	}
 }
-// parse project automator template and extract all environments, i.e. targets
-def Environment = paTemplateXml.Environment
-Environment.IS.'*'.findAll {true}.each { isalias ->
-	if( isalias.name() == 'isalias') {
-//		println("found IS environment" + XmlUtil.serialize(env))
-		isTargets.add([alias: isalias.@name])
+
+paTemplateXml.Projects.Project.DeploymentSet.findAll {true}.each { ds ->
+	String dsName = ds.@name
+	deploymentSets[dsName] = [:]
+	ds.Composite.findAll{true}.each{ composite ->
+		String compositeName = composite.@name
+		String compositeType = composite.@type
+		String compositeDisplayName = composite.@displayName
+		String compositeSrcAlias = composite.@srcAlias
+		def s = deploymentSets[dsName]
+		s[compositeType] = s[compositeType] ? s[compositeType] : []
+		s[compositeType].add([compositeName: compositeName, compositeDisplayName: compositeDisplayName, compositeSrcAlias: compositeSrcAlias])
 	}
 }
+
+
 // now create a new varsub file for all composites and all environments
-String nxml = varSubXml.bind {
-	Root {
-		def varSubEnvIsDir = new File(varSubEnvDir, "is")
-		if( varSubEnvDir.exists() ) {
-			packages.each { isPackage ->
-				// first check if there is a varsub file for this package and the given target environment
-				println "checking for varsub template '${isPackage.name}.vs.xml' in directory '${varSubEnvIsDir.getAbsolutePath()}'"
-				def packageVarSubFile = new File(varSubEnvIsDir, "${isPackage.name}.vs.xml")
-				if( packageVarSubFile.exists() ) {
-					def p = new XmlSlurper().parse( packageVarSubFile )
-					isTargets.each { isTarget ->
-						println "creating varsub for package '${isPackage.name}' and target '${isTarget.alias}'"
+deploymentMaps.each {  mapName, targetServers ->
+	println "- creating varsub for DeploymentMap ${mapName}"
+	File vsOutputFile = new File(varSubOutputDir, mapName.toString() + ".vs.xml")
+	PrintWriter vsOutputFileWriter = new PrintWriter(new BufferedWriter(new FileWriter(vsOutputFile)));
+	def varSubXml = new StreamingMarkupBuilder()
+	String vsxml = varSubXml.bind {
+		Root {
+			targetServers.each { targetServer ->
+				//				println "- creating varsub for ds ${targetServer.dsName} and targetServer ${targetServer.targetServerName} of type ${targetServer.targetServerType}"
+				def deploymentSetName = targetServer.dsName
+				def targetServerName = targetServer.targetServerName
+				def targetServerType = targetServer.targetServerType
+				//				println "deploymentSetName: ${deploymentSetName}, targetServerName: ${targetServerName}, targetServerType: ${targetServerType}"
+
+				def deploymentSet = deploymentSets[deploymentSetName]
+				deploymentSet[targetServerType].each { composite ->
+					def assetCompositeName = composite.compositeName
+					def compositeDisplayName = composite.compositeDisplayName
+					def compositeSrcAlias = composite.compositeSrcAlias
+					// get varsub xml for this composite
+					def vsTemplateFile = new File(varSubEnvDirs[targetServerType], "${assetCompositeName}.vs.xml")
+					if( vsTemplateFile.exists() ) {
+						def p = new XmlSlurper().parse( vsTemplateFile )
 						DeploementSet(
-							assetCompositeName:"${isPackage.name}",
-							deploymentSetName: 'DeploymentSet',
-							serverAliasName: "${isPackage.srcAlias}",
-							targetServerName: "${isTarget.alias}",
-							targetServerType: "IS") {
-							mkp.yield p.children()
-						}
+								assetCompositeName: assetCompositeName,
+								deploymentSetName: deploymentSetName,
+								serverAliasName: compositeSrcAlias,
+								targetServerName: targetServerName,
+								targetServerType: targetServerType) {
+									mkp.yield p.children()
+								}
 					}
-				} else {
-					println "No varsub file exists for package '${isPackage.name}'. Expected varsub at '${varSubEnvIsDir.getAbsolutePath()}/${isPackage.name}.vs.xml'"
 				}
 			}
-		} else {
-			println "no var sub dir for is assets exists at '" + varSubEnvDir.getAbsolutePath() + "/is'"
 		}
-	  }
+	}
+	println XmlUtil.serialize( vsxml, vsOutputFileWriter )
+	vsOutputFileWriter.close()
+	println "wrote varsub file ${vsOutputFile.getAbsolutePath()}"
 }
-println XmlUtil.serialize( nxml, outputFileWriter )
-
-outputFileWriter.close()
-println "created varsub file '${outputFile}'"
