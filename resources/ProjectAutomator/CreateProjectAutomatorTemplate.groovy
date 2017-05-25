faf73d05cf0dfd19ad02ffde3792b046c2cdee3a
Henning Waack (waa) <Henning.Waack@softwareag.com>
2017-05-25 15:44:34 +0200

    adjusted ivy to load less transient dependent jars; adjusted ProjectAutomatorTemplate creation to honor explicit values in ENV.groovy over default values

--- resources/ProjectAutomator/CreateProjectAutomatorTemplate.groovy
+++ resources/ProjectAutomator/CreateProjectAutomatorTemplate.groovy
@@ -1,30 +1,13 @@
 #!/opt/bpa/groovy-2.4.3/bin/groovy
 package com.softwaerag.gcs.wx.bdas.projectAutomator
 
-//@Grapes([
-//	@Grab(group='commons-logging', module='commons-logging', version='1.2'),
-//	@Grab(group='org.springframework', module='spring-core', version='4.3.5.RELEASE'),
-//	@Grab(group='org.apache.httpcomponents', module='httpcore', version='4.3.3'),
-//	@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.5.0'),
-//	@Grab(group='org.apache.ant', module='ant', version='1.9.8'),
-//	@Grab(group='org.apache.ant', module='ant-launcher', version='1.9.8'),
-//	@Grab(group='org.apache.ivy', module='ivy', version='2.4.0')
-//
-//]
-//)
-//import org.apache.http.entity.FileEntity
-//
-//import groovy.lang.Grab
-//import groovy.lang.Grapes
-//import groovy.swing.SwingBuilder
-//import groovyx.net.http.ContentType
-//import groovyx.net.http.Method
 import groovy.xml.dom.DOMCategory
 import groovy.xml.MarkupBuilder
 import groovy.xml.StreamingMarkupBuilder
 import groovy.xml.XmlUtil
 
-def cli = new CliBuilder (usage:'createdeployerproject.groovy -r REPOSITORY_PATH -f OUTPUTFILE -p PROJECT -t TARGETSUFFIX')
+def cli = new CliBuilder (usage:'createdeployerproject.groovy [options]')
+// command line argument line example: -r c:\Users\Administrator\github\henning-sagdevops-ci-assets\tmp\fbr\Henning-local-sagdevops-ci-assets_fbrRepo -f out.xml -p BDA_TEST_Henning-local-sagdevops-ci-assets -t TEST -d localhost:5555 -u Administrator -s manage -e c:\Users\Administrator\github\henning-webmethods-sample-project-layout\ENV.groovy -repoName repoName -splitDeploymentSets false
 cli.with {
 	h longOpt:'help', 'Usage information'
 	r longOpt:'repository',argName:'repository', args:1, 'Path to ABE file based repository'
@@ -41,11 +24,10 @@ cli.with {
 def opts = cli.parse(args)
 org.codehaus.groovy.ant.Groovy
 if(!opts) return
-	if(opts.help) {
-		cli.usage()
-		return
-	}
-
+if(opts.help) {
+	cli.usage()
+	return
+}
 
 assert opts
 assert opts.r
@@ -57,7 +39,6 @@ assert opts.u
 assert opts.s
 assert opts.e
 assert opts.repoName
-
 
 project = opts.p
 def repo = opts.r
@@ -102,8 +83,11 @@ config = configSlurper.parse(new File(environments).toURL())
 
 // first parse the file-based repository
 
+def repoDir = new File(repo)
+assert repoDir.exists() : "Repository directory '${repo}' does not exist"
+
 // we only support packages, processes and MWS projects currently...
-new File(repo).eachDirRecurse() { dir ->
+repoDir.eachDirRecurse() { dir ->
 	dir.eachFileMatch(~/.*.acdl/) { file ->
 		def doc  = groovy.xml.DOMBuilder.parse(new FileReader(file))
 		def asset_composite = doc.documentElement
@@ -143,7 +127,7 @@ def createISOnlyDeploymentMapSetMappingAndDeploymentCandidate(splitDeploymentSet
 				config.IntegrationServers.keySet().each {
 					def isAliasName = it.toString()
 					DeploymentMap(description:"Deployment Map for IS DeploymentSet to IS node ${isAliasName}", name:"IS_DeploymentMap_${isAliasName}")
-				}				
+				}
 				config.IntegrationServers.keySet().each {
 					def isAliasName = it.toString()
 					MapSetMapping(mapName: "IS_DeploymentMap_${isAliasName}", setName: "IS_DeploymentSet_${isAliasName}") {
@@ -278,7 +262,13 @@ def nxml = xml.bind() {
 			if(config.IntegrationServers.size() > 0 ) {
 				IS {
 					config.IntegrationServers.each { name, isConfig ->
-						def integ = isConfig + config.IntegrationServer.defaults;
+						/*
+						 * Merge explicit values in isConfig (ConfigSlurfer ConfigObject) with default
+						 * values in config.IntegrationServer.defaults (hashmap), then merge again
+						 * with explicit values (isConfig), so that default values do not replace
+						 * explicit values, but only append
+						 */
+						def integ = isConfig + config.IntegrationServer.defaults + isConfig;
 						isalias(name: "${target}_${name}") {
 							host(integ.host)
 							port(integ.port)
@@ -299,7 +289,7 @@ def nxml = xml.bind() {
 			if(config.ProcessModels.size() > 0 ) {
 				ProcessModel {
 					config.ProcessModels.each { name, bpmConfig ->
-						def bpm = bpmConfig + config.ProcessModel.defaults;
+						def bpm = bpmConfig + config.ProcessModel.defaults + bpmConfig
 						pmalias(name: "${target}_${name}") {
 							host(bpm.host)
 							port(bpm.port)
@@ -319,7 +309,7 @@ def nxml = xml.bind() {
 			if(config.MWS.size() > 0 ) {
 				MWS {
 					config.MWS.each { name, mwsConfig ->
-						def mwsConfigMerged = mwsConfig + config.MyWebmethodsServer.defaults;
+						def mwsConfigMerged = mwsConfig + config.MyWebmethodsServer.defaults + mwsConfig
 						mwsalias(name: "${target}_${name}") {
 							host(mwsConfigMerged.host)
 							port(mwsConfigMerged.port)
